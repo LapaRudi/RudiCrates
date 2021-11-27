e8e41dd3bf0c883cadcbe8109f2a0afdb530ff46
LapaRudi <82232367+LapaRudi@users.noreply.github.com>
2021-11-28 00:31:40 +0100

    0.9.1

--- src/main/java/de/laparudi/rudicrates/commands/SQLKeyCommand.java
+++ src/main/java/de/laparudi/rudicrates/commands/SQLKeyCommand.java
@@ -6,6 +6,7 @@ import de.laparudi.rudicrates.crate.CrateUtils;
 import de.laparudi.rudicrates.mysql.SQLUtils;
 import de.laparudi.rudicrates.utils.Messages;
 import de.laparudi.rudicrates.utils.UUIDFetcher;
+import de.laparudi.rudicrates.utils.items.ItemManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -17,14 +18,8 @@ import org.bukkit.util.StringUtil;
 import javax.annotation.Nonnull;
 import java.util.*;
 
-public class SQLKeyCommand implements CommandExecutor, TabCompleter {
-    
-    private void createIfNotExists(UUID uuid) {
-        if(!RudiCrates.getPlugin().getMySQL().playerExists(uuid)) {
-            RudiCrates.getPlugin().getMySQL().createPlayer(uuid);
-        }
-    }
-    
+public class SQLKeyCommand extends ItemManager implements CommandExecutor, TabCompleter {
+
     private void sendHelp(CommandSender sender) {
         sender.sendMessage(Messages.PREFIX + "§cFehlerhafte Syntax, benutze /key für eine Übersicht.");
     }
@@ -38,7 +33,7 @@ public class SQLKeyCommand implements CommandExecutor, TabCompleter {
 
         if (args.length == 0) {
             sender.sendMessage("");
-            sender.sendMessage(Messages.PREFIX + "§f/key add §7<§fSpieler§7> <§fCrate§7> <§fAnzahl§7>");
+            sender.sendMessage(Messages.PREFIX + "§f/key add §7<§fSpieler§7> <§fCrate§7> <§fAnzahl§7> [§fitem§7]");
             sender.sendMessage(Messages.PREFIX + "§f/key set §7<§fSpieler§7> <§fCrate§7> <§fAnzahl§7>");
             sender.sendMessage(Messages.PREFIX + "§f/key remove §7<§fSpieler§7> <§fCrate§7> <§fAnzahl§7>");
             sender.sendMessage(Messages.PREFIX + "§f/key info §7<§fSpieler§7>");
@@ -54,21 +49,24 @@ public class SQLKeyCommand implements CommandExecutor, TabCompleter {
                 return true;
             }
 
-            createIfNotExists(uuid);
-            
             final String name = UUIDFetcher.getName(uuid);
             final Player target = Bukkit.getPlayer(uuid);
-            
+
             if (args[0].equalsIgnoreCase("info")) {
                 sender.sendMessage("");
                 sender.sendMessage(Messages.PREFIX + "§fCrates von §c" + name + "§f:");
-                Arrays.stream(CrateUtils.getCrates()).forEach(crate -> sender.sendMessage(Messages.PREFIX + crate.getDisplayname() + " §8» §a" + SQLUtils.getCrateAmount(uuid, crate)));
+
+                Arrays.stream(CrateUtils.getCrates()).forEach(crate -> {
+                    final int keyAmount = target != null ? RudiCrates.getPlugin().getCrateUtils().getKeyItemAmount(target, crate) + SQLUtils.getCrateAmount(uuid, crate) : SQLUtils.getCrateAmount(uuid, crate);
+                    sender.sendMessage(Messages.PREFIX + crate.getDisplayname() + " §8» §a" + keyAmount);
+                });
                 sender.sendMessage("");
 
             } else if (args[0].equalsIgnoreCase("reset")) {
-                if (RudiCrates.getPlugin().getMySQL().resetPlayer(uuid)) {
+                if (RudiCrates.getPlugin().getMySQL().playerExists(uuid)) {
+                    RudiCrates.getPlugin().getMySQL().resetPlayer(uuid);
                     sender.sendMessage(Messages.PREFIX + "§7Die Crates von §c" + name + "§7 wurden zurückgesetzt.");
-                    if (target != null && target != sender) target.sendMessage(Messages.PREFIX + "§7Deine Crates wurden zurückgesetzt.");
+                    if(target != null && target != sender) target.sendMessage(Messages.PREFIX + "§7Deine Crates wurden zurückgesetzt.");
 
                 } else
                     sender.sendMessage(Messages.PREFIX + "§7Dieser Spieler wurde nicht gefunden.");
@@ -77,7 +75,8 @@ public class SQLKeyCommand implements CommandExecutor, TabCompleter {
             return true;
         }
 
-        if (args.length == 4) {
+        if (args.length == 4 || args.length == 5) {
+            final boolean item = args.length == 5 && args[4].equalsIgnoreCase("item");
             final int amount;
 
             try {
@@ -103,33 +102,44 @@ public class SQLKeyCommand implements CommandExecutor, TabCompleter {
 
             final String crateName = crate.getDisplayname();
             final UUID uuid = UUIDFetcher.getUUID(args[1]);
-            
-            if(uuid == null) {
+
+            if(uuid == null || !RudiCrates.getPlugin().getMySQL().playerExists(uuid)) {
                 sender.sendMessage(Messages.PLAYER_NOT_FOUND.toString());
                 return true;
             }
 
-            createIfNotExists(uuid);
-            
             final String name = UUIDFetcher.getName(uuid);
             final Player target = Bukkit.getPlayer(uuid);
+            final boolean online = target != null;
 
             if (args[0].equalsIgnoreCase("add")) {
+                if(item) {
+                    if(!online) {
+                        sender.sendMessage(Messages.PREFIX + "§cDer angegebene Spieler muss online sein, um ihm Schlüssel-Items zu geben.");
+                        return true;
+                    }
+
+                    target.getInventory().addItem(getCrateKeyItem(crate, amount));
+                    sender.sendMessage(Messages.PREFIX + "§7Du hast §c" + name + "§f " + amount + "x " + crateName + "§f (Item) §7gegeben.");
+                    if(sender != target) target.sendMessage(Messages.PREFIX + "§7Du hast §f" + amount + "x " + crateName + "§f (Item) §7erhalten.");
+                    return true;
+                }
+
                 SQLUtils.addCrates(uuid, crate, amount);
                 sender.sendMessage(Messages.PREFIX + "§7Du hast §c" + name + "§f " + amount + "x " + crateName + "§7 gegeben.");
-                if (target != null && target != sender) target.sendMessage(Messages.PREFIX + "§7Du hast §f" + amount + "x " + crateName + "§7 erhalten.");
+                if (online && target != sender) target.sendMessage(Messages.PREFIX + "§7Du hast §f" + amount + "x " + crateName + "§7 erhalten.");
 
             } else if (args[0].equalsIgnoreCase("remove")) {
                 SQLUtils.removeCrates(uuid, crate, amount);
                 sender.sendMessage(Messages.PREFIX + "§7Du hast §c" + name + "§f " + amount + "x " + crateName + "§7 genommen.");
-                if (target != null && target != sender) target.sendMessage(Messages.PREFIX + "§7Dir wurden §f" + amount + "x " + crateName + "§7 genommen.");
+                if (online && target != sender) target.sendMessage(Messages.PREFIX + "§7Dir wurden §f" + amount + "x " + crateName + "§7 genommen.");
 
             } else if (args[0].equalsIgnoreCase("set")) {
                 SQLUtils.setCrateAmount(uuid, crate, amount);
                 sender.sendMessage(Messages.PREFIX + "§7Du hast §c" + name + "'s §f" + crateName + "§7 Anzahl auf §f" + amount + "§7 gesetzt.");
-                if (target != null && target != sender) target.sendMessage(Messages.PREFIX + "§7Deine §f" + crateName + "§7 Anzahl wurde auf §f" + amount + "§7 gesetzt.");
+                if (online && target != sender) target.sendMessage(Messages.PREFIX + "§7Deine §f" + crateName + "§7 Anzahl wurde auf §f" + amount + "§7 gesetzt.");
             }
-        
+
         } else
             sendHelp(sender);
         return true;
@@ -137,26 +147,31 @@ public class SQLKeyCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, @Nonnull Command command, @Nonnull String s, @Nonnull String[] args) {
-        if (!sender.hasPermission("rudicrates.key")) return null;
+        if(!sender.hasPermission("rudicrates.key")) return null;
+
         final List<String> complete = new ArrayList<>();
         final String[] commandArgs = new String[] { "add", "set", "remove", "info", "reset" };
         final List<String> crateArgs = new ArrayList<>();
         Arrays.stream(CrateUtils.getCrates()).forEach(crate -> crateArgs.add(crate.getName()));
 
-        if (args.length == 1) {
+        if(args.length == 1) {
             StringUtil.copyPartialMatches(args[0], Arrays.asList(commandArgs), complete);
             return complete;
 
-        } else if (args.length == 3) {
-            if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("set")) {
+        } else if(args.length == 2) {
+            return null; // Damit Spielernamen angezeigt werden
+
+        } else if(args.length == 3) {
+            if(args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("set")) {
                 StringUtil.copyPartialMatches(args[2], crateArgs, complete);
                 return complete;
             }
-            
-        } else if (args.length == 4) {
-            return Collections.emptyList();
+
+        } else if(args.length == 5) {
+            StringUtil.copyPartialMatches(args[4], Collections.singleton("item"), complete);
+            return complete;
         }
 
-        return null;
+        return Collections.emptyList();
     }
 }
